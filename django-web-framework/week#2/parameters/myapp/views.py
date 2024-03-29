69492948634368097a0fc8ae27ff55c8c3047a7f
Ron Inouye <inouye165@gmail.com>
2024-03-29 06:44:06 -0700

    morning commits

---
+++ django-web-framework/week#2/parameters/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse 
+def pathview(request, name, id): 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+
+def qryview(request): 
+    name = request.GET['name'] 
+    id = request.GET['id'] 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+
+def showform(request): 
+    return render(request, "form.html") 
